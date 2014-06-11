4b9c1be6418117d79983c225d5794c0053c4ea71
cherub0526 <cherub0526@livemail.tw>
2014-06-11 11:55:40 +0800

    End of Lessoion Three

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140611020857) do
+ActiveRecord::Schema.define(version: 20140611034823) do
 
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
